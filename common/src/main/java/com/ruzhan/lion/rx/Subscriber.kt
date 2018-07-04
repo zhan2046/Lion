44791cb7c6e87ec1c17a3d10435ac20ed88fb71c
ruzhan123 <dev19921116@gmail.com>
2018-07-04 18:27:13 +0800

    update common lib network and other file

---
+++ common/src/main/java/com/ruzhan/lion/rx/Subscriber.kt
@@ -0,0 +1,39 @@
+package com.ruzhan.lion.rx
+
+import android.util.Log
+import io.reactivex.CompletableObserver
+import io.reactivex.MaybeObserver
+import io.reactivex.Observer
+import io.reactivex.SingleObserver
+import io.reactivex.disposables.Disposable
+import org.reactivestreams.Subscription
+
+
+class Subscriber<T> internal constructor() : Observer<T>, SingleObserver<T>, MaybeObserver<T>,
+        CompletableObserver, org.reactivestreams.Subscriber<T> {
+
+    override fun onSubscribe(d: Disposable) {}
+
+    override fun onSuccess(t: T) {}
+
+    override fun onSubscribe(s: Subscription) {
+        s.request(Integer.MAX_VALUE.toLong())
+    }
+
+    override fun onNext(o: T) {}
+
+    override fun onError(e: Throwable) {
+        Log.e(TAG, e.toString())
+    }
+
+    override fun onComplete() {}
+
+    companion object {
+
+        private val TAG = "Subscriber"
+
+        fun <T> create(): Subscriber<in T> {
+            return Subscriber()
+        }
+    }
+}
