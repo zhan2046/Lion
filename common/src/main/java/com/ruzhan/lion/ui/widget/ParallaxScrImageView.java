4b227b9f8c8011dda3765f7156952541aa7ef321
ruzhan123 <dev19921116@gmail.com>
2018-07-05 11:19:13 +0800

    update movie detail layout

---
+++ common/src/main/java/com/ruzhan/lion/ui/widget/ParallaxScrImageView.java
@@ -0,0 +1,174 @@
+package com.ruzhan.lion.ui.widget;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.support.annotation.ColorInt;
+import android.support.annotation.FloatRange;
+import android.util.AttributeSet;
+import android.util.Property;
+
+import com.ruzhan.common.R;
+import com.ruzhan.lion.util.AnimUtils;
+import com.ruzhan.lion.util.ColorUtils;
+
+/**
+ * Created by ruzhan123 on 2018/7/5.
+ */
+public class ParallaxScrImageView extends ForegroundImageView {
+
+    private static final int[] STATE_PINNED = { R.attr.state_pinned };
+    private Paint scrimPaint;
+    private int imageOffset;
+    private int minOffset;
+    private Rect clipBounds = new Rect();
+    private float scrimAlpha = 0f;
+    private float maxScrimAlpha = 1f;
+    private int scrimColor = Color.TRANSPARENT;
+    private float parallaxFactor = -0.5f;
+    private boolean isPinned = false;
+    private boolean immediatePin = false;
+
+    public ParallaxScrImageView(Context context) {
+        super(context);
+    }
+
+    public ParallaxScrImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(attrs);
+    }
+
+    public ParallaxScrImageView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(attrs);
+    }
+
+    private void init(AttributeSet attrs) {
+        final TypedArray a = getContext().obtainStyledAttributes(
+                attrs, R.styleable.ParallaxScrimageView);
+        if (a.hasValue(R.styleable.ParallaxScrimageView_scrimColor)) {
+            scrimColor = a.getColor(R.styleable.ParallaxScrimageView_scrimColor, scrimColor);
+        }
+        if (a.hasValue(R.styleable.ParallaxScrimageView_scrimAlpha)) {
+            scrimAlpha = a.getFloat(R.styleable.ParallaxScrimageView_scrimAlpha, scrimAlpha);
+        }
+        if (a.hasValue(R.styleable.ParallaxScrimageView_maxScrimAlpha)) {
+            maxScrimAlpha = a.getFloat(R.styleable.ParallaxScrimageView_maxScrimAlpha,
+                    maxScrimAlpha);
+        }
+        if (a.hasValue(R.styleable.ParallaxScrimageView_parallaxFactor)) {
+            parallaxFactor = a.getFloat(R.styleable.ParallaxScrimageView_parallaxFactor,
+                    parallaxFactor);
+        }
+        a.recycle();
+
+        scrimPaint = new Paint();
+        scrimPaint.setColor(ColorUtils.modifyAlpha(scrimColor, scrimAlpha));
+    }
+
+    public static final Property<ParallaxScrImageView, Integer> OFFSET =
+            AnimUtils.createIntProperty(new AnimUtils.IntProp<ParallaxScrImageView>("offset") {
+                @Override
+                public void set(ParallaxScrImageView parallaxScrimageView, int offset) {
+                    parallaxScrimageView.setOffset(offset);
+                }
+
+                @Override
+                public int get(ParallaxScrImageView parallaxScrimageView) {
+                    return parallaxScrimageView.getOffset();
+                }
+            });
+
+    public int getOffset() {
+        return (int) getTranslationY();
+    }
+
+    public void setOffset(int offset) {
+        offset = Math.max(minOffset, offset);
+        if (offset != getTranslationY()) {
+            setTranslationY(offset);
+            imageOffset = (int) (offset * parallaxFactor);
+            clipBounds.set(0, -offset, getWidth(), getHeight());
+            setClipBounds(clipBounds);
+            setScrimAlpha(Math.min(
+                    ((float) -offset / getMinimumHeight()) * maxScrimAlpha, maxScrimAlpha));
+            postInvalidateOnAnimation();
+        }
+        setPinned(offset == minOffset);
+    }
+
+    public void setScrimAlpha(@FloatRange(from = 0f, to = 1f) float alpha) {
+        if (scrimAlpha != alpha) {
+            scrimAlpha = alpha;
+            scrimPaint.setColor(ColorUtils.modifyAlpha(scrimColor, scrimAlpha));
+            postInvalidateOnAnimation();
+        }
+    }
+
+    public boolean isPinned() {
+        return isPinned;
+    }
+
+    public void setPinned(boolean isPinned) {
+        if (this.isPinned != isPinned) {
+            this.isPinned = isPinned;
+            refreshDrawableState();
+            if (isPinned && immediatePin) {
+                jumpDrawablesToCurrentState();
+            }
+        }
+    }
+
+    public void setScrimColor(@ColorInt int scrimColor) {
+        if (this.scrimColor != scrimColor) {
+            this.scrimColor = scrimColor;
+            postInvalidateOnAnimation();
+        }
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        if (h > getMinimumHeight()) {
+            minOffset = getMinimumHeight() - h;
+        }
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        if (imageOffset != 0) {
+            final int saveCount = canvas.save();
+            canvas.translate(0f, imageOffset);
+            super.onDraw(canvas);
+            canvas.drawRect(0, 0, canvas.getWidth(), canvas.getHeight(), scrimPaint);
+            canvas.restoreToCount(saveCount);
+        } else {
+            super.onDraw(canvas);
+            canvas.drawRect(0, 0, canvas.getWidth(), canvas.getHeight(), scrimPaint);
+        }
+    }
+
+    @Override
+    public int[] onCreateDrawableState(int extraSpace) {
+        final int[] drawableState = super.onCreateDrawableState(extraSpace + 1);
+        if (isPinned) {
+            mergeDrawableStates(drawableState, STATE_PINNED);
+        }
+        return drawableState;
+    }
+
+    public boolean isImmediatePin() {
+        return immediatePin;
+    }
+
+    /**
+     * As the pinned state is designed to work with a {@see StateListAnimator}, we may want to short
+     * circuit this animation in certain situations e.g. when flinging a list.
+     */
+    public void setImmediatePin(boolean immediatePin) {
+        this.immediatePin = immediatePin;
+    }
+}
--- common/src/main/java/com/ruzhan/lion/util/AnimUtils.java
+++ common/src/main/java/com/ruzhan/lion/util/AnimUtils.java
@@ -1,6 +1,9 @@
 package com.ruzhan.lion.util;
 
 import android.content.Context;
+import android.os.Build;
+import android.util.IntProperty;
+import android.util.Property;
 import android.view.animation.AnimationUtils;
 import android.view.animation.Interpolator;
 
@@ -19,5 +22,53 @@ public class AnimUtils {
                     android.R.interpolator.fast_out_slow_in);
         }
         return fastOutSlowIn;
+    }
+
+    /**
+     * A delegate for creating a {@link Property} of <code>int</code> type.
+     */
+    public static abstract class IntProp<T> {
+
+        public final String name;
+
+        public IntProp(String name) {
+            this.name = name;
+        }
+
+        public abstract void set(T object, int value);
+        public abstract int get(T object);
+    }
+
+    /**
+     * The animation framework has an optimization for <code>Properties</code> of type
+     * <code>int</code> but it was only made public in API24, so wrap the impl in our own type
+     * and conditionally create the appropriate type, delegating the implementation.
+     */
+    public static <T> Property<T, Integer> createIntProperty(final IntProp<T> impl) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            return new IntProperty<T>(impl.name) {
+                @Override
+                public Integer get(T object) {
+                    return impl.get(object);
+                }
+
+                @Override
+                public void setValue(T object, int value) {
+                    impl.set(object, value);
+                }
+            };
+        } else {
+            return new Property<T, Integer>(Integer.class, impl.name) {
+                @Override
+                public Integer get(T object) {
+                    return impl.get(object);
+                }
+
+                @Override
+                public void set(T object, Integer value) {
+                    impl.set(object, value);
+                }
+            };
+        }
     }
 }
