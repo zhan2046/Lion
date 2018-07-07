45e47b8cecf46fc4a7824d1e40662f6109ae6fca
ruzhan123 <dev19921116@gmail.com>
2018-07-07 13:56:36 +0800

    add home tool bar animation

--- app/src/main/java/com/ruzhan/lion/ui/home/HomeFragment.kt
+++ app/src/main/java/com/ruzhan/lion/ui/home/HomeFragment.kt
@@ -2,13 +2,19 @@ package com.ruzhan.lion.ui.home
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.app.AppCompatActivity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.ViewTreeObserver
+import android.widget.TextView
 import com.ruzhan.favourites.FavouritesFragment
 import com.ruzhan.lion.R
+import com.ruzhan.lion.util.AnimUtils
 import com.ruzhan.movie.MovieListFragment
 import kotlinx.android.synthetic.main.frag_home.*
+
+
 
 
 /**
@@ -35,6 +41,9 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val activity = activity as AppCompatActivity?
+        activity!!.setSupportActionBar(tool_bar)
+
         bottom_navigation.setOnNavigationItemSelectedListener {
             if (bottom_navigation.selectedItemId != it.itemId) {
                 replaceFragment(it.itemId)
@@ -44,6 +53,13 @@ class HomeFragment : Fragment() {
 
         // normal show MovieListFragment
         replaceFragment(R.id.movie)
+
+        tool_bar.viewTreeObserver.addOnGlobalLayoutListener(object: ViewTreeObserver.OnGlobalLayoutListener {
+            override fun onGlobalLayout() {
+                tool_bar.viewTreeObserver.removeOnGlobalLayoutListener(this)
+                animateToolbar()
+            }
+        })
     }
 
     private fun replaceFragment(tabId: Int) {
@@ -92,6 +108,24 @@ class HomeFragment : Fragment() {
 
         if (!fm.isDestroyed) {
             transaction.commitAllowingStateLoss()
+        }
+    }
+
+    private fun animateToolbar() {
+        // this is gross but toolbar doesn't expose it's children to animate them :(
+        val t = tool_bar.getChildAt(0)
+        if (t != null && t is TextView) {
+
+            // fade in and space out the title.  Animating the letterSpacing performs horribly so
+            // fake it by setting the desired letterSpacing then animating the scaleX ¯\_(ツ)_/¯
+            t.alpha = 0f
+            t.scaleX = 0.8f
+
+            t.animate()
+                    .alpha(1f)
+                    .scaleX(1f)
+                    .setStartDelay(300)
+                    .setDuration(900).interpolator = AnimUtils.getFastOutSlowInInterpolator(activity)
         }
     }
 }
