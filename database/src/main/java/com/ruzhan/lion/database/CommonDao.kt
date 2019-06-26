ad92f18d53eaee80e70f01579da13cc6bdfb6958
ruzhan123 <dev19921116@gmail.com>
2019-06-26 18:06:36 +0800

    add common database module

---
+++ database/src/main/java/com/ruzhan/lion/database/CommonDao.kt
@@ -0,0 +1,17 @@
+package com.ruzhan.lion.database
+
+import android.arch.persistence.room.Dao
+import android.arch.persistence.room.Insert
+import android.arch.persistence.room.OnConflictStrategy
+import android.arch.persistence.room.Query
+import io.reactivex.Flowable
+
+@Dao
+interface CommonDao {
+
+    @Query("SELECT * FROM common_model WHERE id LIKE :id")
+    fun getCommonModel(id: Int): Flowable<CommonModel>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertCommonModel(commonModel: CommonModel)
+}
