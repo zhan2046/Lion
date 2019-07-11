5450747c0757fb6b61908969c48406b8d0d5171e
ruzhan123 <dev19921116@gmail.com>
2019-07-11 10:19:35 +0800

    add web video fragment

--- movie/src/main/java/com/ruzhan/movie/video/WebVideoActivity.kt
+++ movie/src/main/java/com/ruzhan/movie/video/WebVideoActivity.kt
@@ -1,26 +1,19 @@
 package com.ruzhan.movie.video
 
-import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Intent
 import android.graphics.PixelFormat
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.view.View
 import android.view.Window
 import android.view.WindowManager
 import com.ruzhan.movie.R
-import com.tencent.smtt.sdk.WebChromeClient
-import com.tencent.smtt.sdk.WebView
-import com.tencent.smtt.sdk.WebViewClient
-import kotlinx.android.synthetic.main.activity_web_video.*
 
 class WebVideoActivity : AppCompatActivity() {
 
     companion object {
 
         private const val VIDEO_URL: String = "URL"
-        private const val MAX_PROGRESS = 80
 
         @JvmStatic
         fun launch(activity: Activity, url: String) {
@@ -30,49 +23,21 @@ class WebVideoActivity : AppCompatActivity() {
         }
     }
 
-    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         requestWindowFeature(Window.FEATURE_NO_TITLE)
         window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN)
         window.setFormat(PixelFormat.TRANSLUCENT)
-
-        setContentView(R.layout.activity_web_video)
-
-        val webSettings = web_view.settings
-        webSettings.javaScriptEnabled = true
-        webSettings.domStorageEnabled = true
-
-        web_view.webChromeClient = object : WebChromeClient() {
-            override fun onProgressChanged(view: WebView, newProgress: Int) {
-                if (load_progress.visibility != View.VISIBLE && newProgress < MAX_PROGRESS) {
-                    load_progress.visibility = View.VISIBLE
-
-                } else if (newProgress > MAX_PROGRESS) {
-                    load_progress.visibility = View.INVISIBLE
-                }
-            }
-        }
-        web_view.webViewClient = WebViewClient()
+        setContentView(R.layout.container)
 
         val url = intent.getStringExtra(VIDEO_URL)
-        web_view.loadUrl(url)
-    }
-
-    override fun onResume() {
-        web_view.onResume()
-        super.onResume()
-    }
-
-    override fun onPause() {
-        web_view.onPause()
-        super.onPause()
-    }
-
-    override fun onDestroy() {
-        web_view.destroy()
-        super.onDestroy()
+        if (savedInstanceState == null) {
+            val webVideoFragment = WebVideoFragment.newInstance(url)
+            supportFragmentManager
+                    .beginTransaction()
+                    .add(R.id.container, webVideoFragment, "WebVideoFragment")
+                    .commit()
+        }
     }
 }
---
+++ movie/src/main/java/com/ruzhan/movie/video/WebVideoFragment.kt
@@ -0,0 +1,83 @@
+package com.ruzhan.movie.video
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ruzhan.movie.R
+import com.tencent.smtt.sdk.WebChromeClient
+import com.tencent.smtt.sdk.WebView
+import com.tencent.smtt.sdk.WebViewClient
+import kotlinx.android.synthetic.main.frag_web_video.*
+
+class WebVideoFragment : Fragment() {
+
+    companion object {
+
+        private const val VIDEO_URL: String = "URL"
+        private const val MAX_PROGRESS = 80
+
+        @JvmStatic
+        fun newInstance(url: String): WebVideoFragment {
+            val args = Bundle()
+            args.putString(VIDEO_URL, url)
+            val frag = WebVideoFragment()
+            frag.arguments = args
+            return frag
+        }
+    }
+
+    private var url = ""
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        url = arguments?.getString(VIDEO_URL) ?: ""
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.frag_web_video, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initData()
+        web_view.loadUrl(url)
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun initData() {
+        val webSettings = web_view.settings
+        webSettings.javaScriptEnabled = true
+        webSettings.domStorageEnabled = true
+
+        web_view.webChromeClient = object : WebChromeClient() {
+            override fun onProgressChanged(view: WebView, newProgress: Int) {
+                if (load_progress.visibility != View.VISIBLE && newProgress < MAX_PROGRESS) {
+                    load_progress.visibility = View.VISIBLE
+
+                } else if (newProgress > MAX_PROGRESS) {
+                    load_progress.visibility = View.INVISIBLE
+                }
+            }
+        }
+        web_view.webViewClient = WebViewClient()
+    }
+
+    override fun onResume() {
+        web_view.onResume()
+        super.onResume()
+    }
+
+    override fun onPause() {
+        web_view.onPause()
+        super.onPause()
+    }
+
+    override fun onDestroyView() {
+        web_view.destroy()
+        super.onDestroyView()
+    }
+}
