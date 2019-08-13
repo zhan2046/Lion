ce8b7f5737235162e2727762a06494ad4a99e804
ruzhan123 <dev19921116@gmail.com>
2019-08-13 15:42:31 +0800

    update tencent x5 webview jar

--- app/src/main/java/com/ruzhan/lion/ui/MainApplication.kt
+++ app/src/main/java/com/ruzhan/lion/ui/MainApplication.kt
@@ -2,9 +2,11 @@ package com.ruzhan.lion.ui
 
 import android.app.Application
 import android.content.Context
+import android.util.Log
 import androidx.multidex.MultiDex
 import com.lion.font.FontHelper
 import com.ruzhan.lion.util.ResUtils
+import com.tencent.smtt.sdk.QbSdk
 
 
 class MainApplication : Application() {
@@ -18,5 +20,16 @@ class MainApplication : Application() {
         super.onCreate()
         ResUtils.init(this)
         FontHelper.init(this)
+        val cb = object : QbSdk.PreInitCallback {
+
+            override fun onViewInitFinished(arg0: Boolean) {
+                Log.d("lion", " onViewInitFinished is $arg0")
+            }
+
+            override fun onCoreInitFinished() {
+                // do nothing
+            }
+        }
+        QbSdk.initX5Environment(this, cb)
     }
 }
--- movie/src/main/java/com/ruzhan/movie/video/WebVideoFragment.kt
+++ movie/src/main/java/com/ruzhan/movie/video/WebVideoFragment.kt
@@ -2,7 +2,6 @@ package com.ruzhan.movie.video
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
