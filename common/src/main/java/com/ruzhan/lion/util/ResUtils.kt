d1e60705525b85de19a90e7d95280deaf8569180
ruzhan123 <dev19921116@gmail.com>
2019-08-18 21:44:21 +0800

    update resource utils

--- common/src/main/java/com/ruzhan/lion/util/ResUtils.kt
+++ common/src/main/java/com/ruzhan/lion/util/ResUtils.kt
@@ -1,6 +1,11 @@
 package com.ruzhan.lion.util
 
 import android.app.Application
+import android.content.Context
+import android.graphics.drawable.Drawable
+import android.widget.ImageView
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.DrawableCompat
 
 object ResUtils {
 
@@ -13,4 +18,48 @@ object ResUtils {
 
     @JvmStatic
     fun getApp(): Application = application
+
+
+    @JvmStatic
+    fun getString(strId: Int): String {
+        return if (strId <= 0) {
+            ""
+        } else getApp().resources.getString(strId) ?: ""
+    }
+
+    @JvmStatic
+    fun getStringArray(strArrayId: Int): Array<String>? {
+        return if (strArrayId <= 0) {
+            null
+        } else getApp().resources.getStringArray(strArrayId)
+    }
+
+    @JvmStatic
+    fun getString(strId: Int, vararg formatArgs: Any): String {
+        return if (strId <= 0) {
+            ""
+        } else getApp().resources.getString(strId, *formatArgs) ?: ""
+    }
+
+    @JvmStatic
+    fun getColor(colorId: Int): Int {
+        return if (colorId <= 0) {
+            -1
+        } else getApp().resources.getColor(colorId)
+    }
+
+    @JvmStatic
+    fun getDrawable(drawableId: Int): Drawable? {
+        return if (drawableId <= 0) {
+            null
+        } else getApp().resources.getDrawable(drawableId)
+    }
+
+    @JvmStatic
+    fun setTint(context: Context, DrawableRes: Int, tintColor: Int, imageView: ImageView) {
+        val up = ContextCompat.getDrawable(context, DrawableRes)
+        val drawableUp = DrawableCompat.wrap(up!!)
+        DrawableCompat.setTint(drawableUp, ContextCompat.getColor(context, tintColor))
+        imageView.setImageDrawable(drawableUp)
+    }
 }
