28fe10754e7fc01afa240507b7e15f997ddcf502
ruzhan123 <dev19921116@gmail.com>
2019-11-26 16:30:46 +0800

    update android version to 29

--- common/src/main/java/com/ruzhan/lion/util/ResUtils.kt
+++ common/src/main/java/com/ruzhan/lion/util/ResUtils.kt
@@ -18,48 +18,4 @@ object ResUtils {
 
     @JvmStatic
     fun getApp(): Application = application
-
-
-    @JvmStatic
-    fun getString(strId: Int): String {
-        return if (strId <= 0) {
-            ""
-        } else getApp().resources.getString(strId) ?: ""
-    }
-
-    @JvmStatic
-    fun getStringArray(strArrayId: Int): Array<String>? {
-        return if (strArrayId <= 0) {
-            null
-        } else getApp().resources.getStringArray(strArrayId)
-    }
-
-    @JvmStatic
-    fun getString(strId: Int, vararg formatArgs: Any): String {
-        return if (strId <= 0) {
-            ""
-        } else getApp().resources.getString(strId, *formatArgs) ?: ""
-    }
-
-    @JvmStatic
-    fun getColor(colorId: Int): Int {
-        return if (colorId <= 0) {
-            -1
-        } else ContextCompat.getColor(getApp(), colorId)
-    }
-
-    @JvmStatic
-    fun getDrawable(drawableId: Int): Drawable? {
-        return if (drawableId <= 0) {
-            null
-        } else ContextCompat.getDrawable(getApp(), drawableId)
-    }
-
-    @JvmStatic
-    fun setTint(context: Context, DrawableRes: Int, tintColor: Int, imageView: ImageView) {
-        val up = ContextCompat.getDrawable(context, DrawableRes)
-        val drawableUp = DrawableCompat.wrap(up!!)
-        DrawableCompat.setTint(drawableUp, ContextCompat.getColor(context, tintColor))
-        imageView.setImageDrawable(drawableUp)
-    }
 }
--- movie/src/main/java/com/ruzhan/movie/detail/fragment/MovieDetailFragment.kt
+++ movie/src/main/java/com/ruzhan/movie/detail/fragment/MovieDetailFragment.kt
@@ -47,7 +47,7 @@ class MovieDetailFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        movie = arguments?.getParcelable(MOVIE) as Movie
+        movie = arguments?.getParcelable(MOVIE)!!
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
