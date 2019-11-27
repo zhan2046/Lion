95cb0bc44fb10421151376980b5ff5af54b971c6
ruzhan123 <dev19921116@gmail.com>
2019-11-27 16:06:36 +0800

    opt scale title text view default value

--- movie/src/main/java/com/ruzhan/movie/widget/ScaleTransitionPagerTitleView.kt
+++ movie/src/main/java/com/ruzhan/movie/widget/ScaleTransitionPagerTitleView.kt
@@ -4,16 +4,21 @@ import android.content.Context
 import net.lucode.hackware.magicindicator.buildins.commonnavigator.titles.ColorTransitionPagerTitleView
 
 class ScaleTransitionPagerTitleView(context: Context) : ColorTransitionPagerTitleView(context) {
-    var minScale = 0.75f
+
+    companion object {
+        private const val DEFAULT_SCALE = 0.85f
+    }
+
+    var minScale = DEFAULT_SCALE
 
     override fun onEnter(index: Int, totalCount: Int, enterPercent: Float, leftToRight: Boolean) {
-        super.onEnter(index, totalCount, enterPercent, leftToRight)    // 实现颜色渐变
+        super.onEnter(index, totalCount, enterPercent, leftToRight)
         scaleX = minScale + (1.0f - minScale) * enterPercent
         scaleY = minScale + (1.0f - minScale) * enterPercent
     }
 
     override fun onLeave(index: Int, totalCount: Int, leavePercent: Float, leftToRight: Boolean) {
-        super.onLeave(index, totalCount, leavePercent, leftToRight)    // 实现颜色渐变
+        super.onLeave(index, totalCount, leavePercent, leftToRight)
         scaleX = 1.0f + (minScale - 1.0f) * leavePercent
         scaleY = 1.0f + (minScale - 1.0f) * leavePercent
     }
